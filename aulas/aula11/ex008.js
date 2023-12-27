0d84eb4a0ba44e4f9395e61a2be676fdedac9d0d
samuka-santos <139240738+samuka-santos@users.noreply.github.com>
2023-12-27 13:19:17 -0400

    aula12

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,8 @@
+// Com a extensão Node.js Exec no VS Code, aperte F8 para executar (F9 para cancelar execução)
+
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você utrapassou a velocidade permitida. MULTADO!') 
+}
+console.log('Dirija sempre com cinto de segurança!')
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'BR'
+console.log(`Vivendo em ${país}`)
+
+if (país != 'BR') {
+    console.log(`Você é estrangeiro`)
+} else {
+    console.log(`Você é brasileiro`)
+}
